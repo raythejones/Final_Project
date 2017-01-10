20d831e875b94e5f85824af334296fdcdafd9451
Miqbal2587 <miqbal1@stuy.edu>
2017-01-09 21:04:05 -0500

    Update AlgebraCalculator.java

--- AlgebraCalculator.java
+++ AlgebraCalculator.java
@@ -16,7 +16,10 @@ public class AlgebraCalculator extends JFrame{
     private JButton compute;
     private String valueRight,valueLeft, solution;
     Boolean solve=false;
+
+
     // Creates the Pane/Tab
+
     public AlgebraCalculator(){
 	this.setTitle("Algebraic Calculator");
 	//Might not need if we are doing tabs?
@@ -36,13 +39,17 @@ public class AlgebraCalculator extends JFrame{
 	output.setEditable(false);
 	compute= new JButton ("Compute");
 	// Sets the GUI
+
 	algPanel.add(inputLeft);
 	algPanel.add(equals);
 	algPanel.add(inputRight);
 	algPanel.add(compute);
 	algPanel.add(output);
-	//Creaytes the GUI
+
+	//Creates the GUI
+
 	add(algPanel); 
+
 	//Allows us to gather the information from the GUI
 	compute.addActionListener(new ListenCompute());
     }
@@ -52,15 +59,17 @@ class ListenCompute implements ActionListener{
     public void actionPerformed(ActionEvent i){
 	valueLeft=inputLeft.getText();
 	valueRight=inputRight.getText();
-	if (isExpression(valueLeft) && isExpression(valueRight) && isSameVar(valueRight,valueLeft)){
+	if ((isExpression(valueLeft) || isExpression(valueRight)) && isSameVar(valueRight,valueLeft)){
 	    valueLeft=simplify(valueLeft);
 	    valueRight=simplify(valueRight);
+
 	    /* //for polynoymials
 	       if (isExpo(valueLeft) || isExpo(valueRight)){
 	           solvepoly(valueLeft
 	       }
 	       else{
 	    */
+
 	    solution=(solve(valueLeft, valueRight));
 	    output.setText(solution);
 	    inputLeft.setText("");
@@ -68,25 +77,40 @@ class ListenCompute implements ActionListener{
 	}
 	else{
 	    JOptionPane error = new JOptionPane("A valid expression must be inputed in both sides of the equation, as well as the same variable.", JOptionPane.ERROR_MESSAGE);
+	    JDialog message = error.createDialog("InvalidInputException");
+	    message.setAlwaysOnTop(true);
+	    message.setVisible(true);
 	}
     }
+    
     // Checks to see if the query string is an expression (not working as of yet)
+
     public boolean isExpression(String value){
-	char var='*';
+	if (value.equals("")){
+	    return false;
+	}
+	char var=';';
 	for (int x=0; x<value.length();x++){
 	    if (Character.isLetter(value.charAt(x))){
-		if(var=='*'){
+		if(var==';'){
 		    var=value.charAt(x);
 		}
 		else if (var!=value.charAt(x)){
 			return false;
 		    }
+		
 	    }
 	}
-	return true;
-	
+	if (var==';'){
+	    return false;
+	}
+	else{
+	    return true;
+	}
     }
+    
     //Checks to see if the same variable is used
+
     public boolean isSameVar(String exp1, String exp2){
 	char var1='*';
 	char var2='*';
@@ -95,28 +119,61 @@ class ListenCompute implements ActionListener{
 		var1=exp1.charAt(x);
 	    }
 	}
-	for (int x=0; x<exp2.length(); x++){
+	for (int x=0; x<exp2.length()&& hasLetter(exp2); x++){
 	    if(Character.isLetter(exp2.charAt(x))){
 		var2=exp2.charAt(x);
 	    }
 	}
-	if (var1 == var2){
+	if  (var1 == var2 ||
+	     (hasLetter(exp1) || !hasLetter(exp2)) ||
+	     (hasLetter(exp2) || !hasLetter(exp1))){
 	    return true;
 	}
 	else{
 	    return false;
 	}
     }
-    // To simplify long algebraic equations (not working as of yet)
+
+    public boolean hasLetter(String value){
+	boolean has=false;
+	for (int x=0; x<value.length(); x++){
+	    if(Character.isLetter(value.charAt(x))){
+		has=true;
+	    }
+	}
+	return has;
+    }
+    // To simplify long algebraic equations. Follows PEMDAS
+
     public String simplify(String value){
+	//Parenthesis
+	if (value.indexOf('(')>-1 || value.indexOf(')')>-1){
+	    if (value.indexOf(')')==-1 || value.indexOf('(')==-1){
+		JOptionPane error = new JOptionPane("Expected close parenthesis", JOptionPane.ERROR_MESSAGE);
+		JDialog message = error.createDialog("ParenthesisException");
+		message.setAlwaysOnTop(true);
+		message.setVisible(true);
+	    }
+	    else{
+		
+	    }
+	}
+	/* Exponents (Not in use until on to polynomials)
+	else if(value.indexOf("^")>-1){
+
+	}
+	*/
 	return value;
     }
+	
     //Should try to move the constants to one side keeping the variable on the other for monomials expressions, then should either divide or multiply depending on the coefficent. (Working on)
-    public String solve(String exp1, String exp2){
+
+	public String solve(String exp1, String exp2){
 	//Dummy Text
 	return exp1;
     }
 }
+    
     /*
     public String solvePoly(String exp1, String exp2){
 	
