bf2a646ddc88ab050b7d86bc3c9c404867bce43d
Ray Jones <ajones@stuy.edu>
2017-01-16 17:32:27 -0500

    all purpose function works with adding and multiplying

--- CreateHelperFunction.java
+++ CreateHelperFunction.java
@@ -1,12 +1,28 @@
-public class CreateHelperFunction{
+public class CreateHelperFunction {
 
-public static TextToExpression(String theInput){
+public static double evaluate(String eq) {
+        double res = 0.0;
+        String []divPlus = eq.split("\\+");
+        for (String mult : divPlus) {
+            String []divMult = mult.split("\\*");
+            double multRes = 1.0;
+            for (String operand : divMult) {
+                multRes *= Integer.parseInt(operand);
+            }
+            res += multRes;
+        }
+        return res;
+    }
+    
 
 
 
+    public static void main(String[] args) {
+        String eq = "1*5+4*8+9*2";
+        double res = evaluate(eq);
+        System.out.println(res);
+    }
 
-}
-
-public static void main(
+ 
 
 }
