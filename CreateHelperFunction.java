5e97be465166f57e342cfe5356c03a08c49710dc
Ray Jones <ajones@stuy.edu>
2017-01-16 22:34:04 -0500

    did some research, and found a much easier way to do what I've wanted

--- CreateHelperFunction.java
+++ CreateHelperFunction.java
@@ -1,38 +1,24 @@
+import javax.script.*;
+
 public class CreateHelperFunction {
     
-    public static double evaluate(String eq) {
-        double res = 0.0;        
-        String MinusToPlus = eq.replace("-", "+-");
-        String[] splitAddition = MinusToPlus.split("\\+");
-	
-        for (String multipl : splitAddition) {
-            String[] splitMultiplication = multipl.split("\\*");
-            double multRes = 1.0;
-            for (String operand : splitMultiplication) {
-                if (operand.contains("/")) {
-                    String[] splitDivision = operand.split("\\/");
-                    double dividend = Double.parseDouble(splitDivision[0]);
-                    for (int i = 1; i < splitDivision.length; i++) {
-                        dividend /= Double.parseDouble(splitDivision[i]);
-                    }
-                    multRes *= dividend;
-                } else {
-                    multRes *= Double.parseDouble(operand);
-                }
-            }
-            res += multRes;
-        }
-	
-        return res;
-    } 
+    public static Object evaluate(String stringInput) throws Exception{
+       // create a script engine manager
+        ScriptEngineManager factory = new ScriptEngineManager();
+        // create a JavaScript engine
+        ScriptEngine engine = factory.getEngineByName("JavaScript");
+        // evaluate JavaScript code from String
+
+	return engine.eval(stringInput);
     
+	}
     
     
     
     public static void main(String[] args) {
-        String eq = "1*-5+4*8+9*2";
-        double res = evaluate(eq);
-        System.out.println(res);
+  
+
+
     }
     
  
