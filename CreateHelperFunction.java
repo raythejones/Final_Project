2adf7330578e1987b2680b647bf556bd61b4607d
Ray Jones <ajones@stuy.edu>
2017-01-16 19:24:17 -0500

    Works with addition, multiplication, division, and subtraction. Negative numbers are a problem.

--- CreateHelperFunction.java
+++ CreateHelperFunction.java
@@ -1,28 +1,40 @@
 public class CreateHelperFunction {
-
-public static double evaluate(String eq) {
-        double res = 0.0;
-        String []divPlus = eq.split("\\+");
-        for (String mult : divPlus) {
-            String []divMult = mult.split("\\*");
+    
+    public static double evaluate(String eq) {
+        double res = 0.0;        
+        String MinusToPlus = eq.replace("-", "+-");
+        String[] splitAddition = MinusToPlus.split("\\+");
+	
+        for (String multipl : splitAddition) {
+            String[] splitMultiplication = multipl.split("\\*");
             double multRes = 1.0;
-            for (String operand : divMult) {
-                multRes *= Integer.parseInt(operand);
+            for (String operand : splitMultiplication) {
+                if (operand.contains("/")) {
+                    String[] splitDivision = operand.split("\\/");
+                    double dividend = Double.parseDouble(splitDivision[0]);
+                    for (int i = 1; i < splitDivision.length; i++) {
+                        dividend /= Double.parseDouble(splitDivision[i]);
+                    }
+                    multRes *= dividend;
+                } else {
+                    multRes *= Double.parseDouble(operand);
+                }
             }
             res += multRes;
         }
+	
         return res;
-    }
+    } 
     
-
-
-
+    
+    
+    
     public static void main(String[] args) {
-        String eq = "1*5+4*8+9*2";
+        String eq = "1*-5+4*8+9*2";
         double res = evaluate(eq);
         System.out.println(res);
     }
-
+    
  
-
+    
 }
